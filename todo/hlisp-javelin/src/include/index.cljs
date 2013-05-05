d0224cb31b2d1179f78bab339973ab2c45a58bfd
Micha Niskin <asdf@ubergibson.com>
2013-05-05 12:53:49 -0400

    Update to newest hlisp versions
    - Update with newest versions of various HLisp dependencies
    - Using new "n-things capable" thing-looper, so remove code to hide
      deleted items as it's no longer necessary.
    - Minor code cleanup.

--- todo/hlisp-javelin/src/include/index.cljs
+++ todo/hlisp-javelin/src/include/index.cljs
@@ -4,6 +4,7 @@
     [hlisp.reactive.macros        :refer [reactive-attributes]]
     [tailrecursion.javelin.macros :refer [cell]])
   (:require
+    [clojure.string         :as s :refer [blank?]]
     [hlisp.reactive         :as d :refer [thing-looper]]
     [hlisp.util                   :refer [pluralize]]
     [tailrecursion.javelin        :refer [route*]]
@@ -11,53 +12,47 @@
 
 ;; internal ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(declare route state active? deleted? completed?)
-
-(defn- update-state! [todos]
-  (let [grouped (group-by deleted? todos)]
-    (reset! state (into (get grouped false) (get grouped true)))))
+(declare route completed?)
 
 (defn- reactive-info [todos i]
-  (let [cellval #(cell (get (nth todos i) %))]
-    (conj (mapv cellval [:editing :completed :text])
-          (cell (let [todo (nth todos i)]
-                  (and (not (deleted? todo)) 
-                       (or (= "#/" route)
-                           (and (= "#/active" route) (active? todo))
-                           (and (= "#/completed" route) (completed? todo)))))))))
-
-(def deleted?     #(empty? (:text (if (map? %) % (nth @state %)))))
-(def completed?   #(:completed (if (map? %) % (nth @state %))))
-(def active?      #(and (not (deleted? %)) (not (completed? %))))
-(def clear        #(assoc % :editing false :completed false :text "")) 
-(def dfl-state    (vec (repeat 50 {:editing false :completed true :text ""}))) 
+  (let [todo (cell (get-in todos [i]))]
+    [(cell (:editing todo))
+     (cell (:completed todo))
+     (cell (:text todo))
+     (cell (or (= "#/" route)
+               (and (= "#/active" route) (not (completed? todo)))
+               (and (= "#/completed" route) (completed? todo))))]))
 
 ;; public ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def state        (local-storage (cell dfl-state) ::store))
+(def completed?   #(:completed %))
+(def state        (local-storage (cell '[]) ::store))
 (def route        (route* 50 "#/")) 
 (def editing-new  (cell "")) 
-(def live-ones    (cell (filter (complement deleted?) state))) 
-(def completed    (cell (filter completed? live-ones))) 
-(def active       (cell (filter (complement completed?) live-ones)))
+(def completed    (cell (filter completed? state))) 
+(def active       (cell (remove completed? state)))
 (def loop-todos   (thing-looper state reactive-info)) 
 
-(defn new-item! [text]
-  (if (not (empty? text))
-    (update-state! (into [(assoc (clear (peek @state)) :text text)] (pop @state)))))
+(defn add-item! [text]
+  (if-not (blank? text)
+    (let [todo {:editing false :completed false :text text}]
+      (swap! state #(into [todo] (vec %))))))
+
+(defn remove-item! [i]
+  (swap! state #(into (subvec % 0 i) (subvec % (inc i)))))
 
 (defn set-editing! [i v]
-  (update-state! (-> (mapv #(assoc % :editing false) @state)
-                     (assoc-in [i :editing] v))))
-
-(defn set-completed! [i v]
-  (update-state! (assoc-in @state [i :completed] v)))
-
-(defn set-all-completed! [v]
-  (update-state! (mapv #(assoc % :completed v) @state)))
+  (swap! state (fn [s] (-> (mapv #(assoc % :editing false) s)
+                         (assoc-in [i :editing] v)))))
 
 (defn set-text! [i v]
-  (update-state! (assoc-in (vec @state) [i :text] v)))
+  (when-not (blank? v) (swap! state assoc-in [i :text] v)))
+
+(defn set-completed! [i v]
+  (swap! state assoc-in [i :completed] v))
+
+(defn set-all-completed! [v]
+  (swap! state (fn [s] (mapv #(assoc % :completed v) s))))
 
 (defn clear-completed! []
-  (update-state! (mapv #(if (completed? %) (clear %) %) @state)))
+  (swap! state #(vec (remove completed? %))))
